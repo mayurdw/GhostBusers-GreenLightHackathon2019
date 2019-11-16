0de868047082e1f3685df9263601bc1685928446
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2019-11-16 16:09:26 +1300

    Problem Activity

--- app/src/main/java/com/example/ghostbusers/MainActivity.java
+++ app/src/main/java/com/example/ghostbusers/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ghostbusers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startApp(View view){
-
+        Intent newActivity = new Intent(this, HadAProblemActivity.class);
+        startActivity(newActivity);
     }
 }
